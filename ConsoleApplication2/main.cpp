5b04fa9c681da5a50a9fd0412b2e3e745b3bc3ad
Julio Makdisse Saito <saitodisse@gmail.com>
2013-02-08 10:38:45 -0200

    hello world most simple ever

---
+++ ConsoleApplication2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	std::cout << "------------" << "\n"
+		<< "Hello World!" << "\n"
+		<< "This is good." << "\n"
+		<< "------------" << "\n"
+		<< "I'm doing C++." << "\n\n";
+
+	return 0;
+}
